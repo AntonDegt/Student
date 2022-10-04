4acbc088dd280dc23725888f3ed1d75dc6e49ef7
AntonDegt <108671823+AntonDegt@users.noreply.github.com>
2022-10-04 16:43:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleСSApp;
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Aaa", "Bbb", new DateTime(2004, 09, 11), "Uk");
+            student.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleСSApp
+{
+    class Student
+    {
+        public string Name 
+        { 
+            get { return Name; }
+            set { if (value.Length >= 2) Name = value; }
+        }
+        public string Surname
+        {
+            get { return Surname; }
+            set { if (value.Length >= 2) Surname = value; }
+        }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public List<int> Offsets;
+        public List<int> TermPapers;
+        public List<int> Exam;
+
+        public Student(string Name, string Surname, DateTime DateOfBirth, string Address, string PhoneNumber)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.DateOfBirth = DateOfBirth;
+            this.Address = Address;
+            this.PhoneNumber = PhoneNumber;
+        }
+        public Student(string Name, string Surname, DateTime DateOfBirth, string Address)
+            : this(Name, Surname, DateOfBirth, Address, "") { }
+        public Student(string Name, string Surname, DateTime DateOfBirth)
+            : this(Name, Surname, DateOfBirth, "", "") { }
+
+        public void Print()
+        {
+            Console.WriteLine(Name + " " + Surname + " " + DateOfBirth + " " + Address + " " + PhoneNumber);
+        }
+    }
+}
